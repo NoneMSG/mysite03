e56f6b54567f508cda6db2dc866c9dfa994fdf05
bit-user <princeps90@gmail.com>
2017-06-01 16:14:09 +0900

    logging added

--- src/main/java/com/jx372/mysite/controller/DomainController.java
+++ src/main/java/com/jx372/mysite/controller/DomainController.java
@@ -1,15 +1,20 @@
 package com.jx372.mysite.controller;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+
 @Controller
 public class DomainController {
+	
 	
 	@RequestMapping({"/", "/main"})
 	public String index(){
 		//view resolver bean을 설정해주었기 때문에 URL을 간략하게 사용할 수 있다.
+
 		return "main/index";
 	}
 	
@@ -19,6 +24,7 @@ public class DomainController {
 //	public String hello(){
 //		return "hello world"; 
 //	}
+	
 	//responsebody 어노테이션으로 메시지컨터버테의해서 데이터가 반환되어 브라우저에 뿌려진다.
 	@ResponseBody
 	@RequestMapping("/hello")
